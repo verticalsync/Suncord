8d5baf41ec6c8a26b235fdec936a859f3fcec5e6
verticalsync <window_sync@proton.me>
2024-01-19 21:49:40 +0200

    Get rid of annoying console.log in timezones plugin

--- src/plugins/Timezones/index.tsx
+++ src/plugins/Timezones/index.tsx
@@ -221,8 +221,6 @@ export default definePlugin({
     },
 
     getTimezonesComponent: ({ message }: { message: Message; }) => {
-        console.log(message);
-
         const { showTimezonesInChat, preference } = settings.use(["preference", "showTimezonesInChat"]);
         const [timezone, setTimezone] = React.useState<string | undefined>();
 
