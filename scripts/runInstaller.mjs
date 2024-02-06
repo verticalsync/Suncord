ec1ebceeeea5ee146fadfbce5498241199f40b5c
verticalsync <nightly@riseup.net>
2024-02-06 21:15:55 +0200

    (vc/dev) inject: suppress ugly node error

--- scripts/runInstaller.mjs
+++ scripts/runInstaller.mjs
@@ -118,11 +118,15 @@ const installerBin = await ensureBinary();
 
 console.log("Now running Installer...");
 
-execFileSync(installerBin, {
-    stdio: "inherit",
-    env: {
-        ...process.env,
-        SUNCORD_USER_DATA_DIR: BASE_DIR,
-        SUNCORD_DEV_INSTALL: "1"
-    }
-});
+try {
+    execFileSync(installerBin, {
+        stdio: "inherit",
+        env: {
+            ...process.env,
+            SUNCORD_USER_DATA_DIR: BASE_DIR,
+            SUNCORD_DEV_INSTALL: "1"
+        }
+    });
+} catch {
+    console.error("Something went wrong. Please check the logs above.");
+}
