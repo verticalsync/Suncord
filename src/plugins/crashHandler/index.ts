d2fc12e3eeea0cd4056dd6c72dd100120b749340
verticalsync <60797172+verticalsync@users.noreply.github.com>
2024-02-14 21:36:13 +0200

    vencord -> suncord update message

--- src/plugins/crashHandler/index.ts
+++ src/plugins/crashHandler/index.ts
@@ -110,7 +110,7 @@ export default definePlugin({
             try {
                 if (!hasCrashedOnce) {
                     hasCrashedOnce = true;
-                    maybePromptToUpdate("Uh oh, Discord has just crashed... but good news, there is a Vencord update available that might fix this issue! Would you like to update now?", true);
+                    maybePromptToUpdate("Uh oh, Discord has just crashed... but good news, there is a Suncord update available that might fix this issue! Would you like to update now?", true);
                 }
             } catch { }
 
