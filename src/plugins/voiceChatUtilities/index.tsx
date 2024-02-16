76dc37dd0998edb6b2e417714fd338363e4bf24a
verticalsync <60797172+verticalsync@users.noreply.github.com>
2024-02-16 22:06:05 +0200

    unsupported username

--- src/plugins/voiceChatUtilities/index.tsx
+++ src/plugins/voiceChatUtilities/index.tsx
@@ -125,7 +125,7 @@ const VoiceChannelContext: NavContextMenuPatchCallback = (children, { channel }:
 export default definePlugin({
     name: "VoiceChatUtilities",
     description: "This plugin allows you to perform multiple actions on an entire channel (move, mute, disconnect, etc.) (originally by dutake)",
-    authors: [{ name: "! 𝕯'𝖆𝖒𝖘", id: 769939285792653325n }, Devs.D3SOX],
+    authors: [{ name: "Unsupported username", id: 769939285792653325n }, Devs.D3SOX],
 
     start() {
         addContextMenuPatch("channel-context", VoiceChannelContext);
