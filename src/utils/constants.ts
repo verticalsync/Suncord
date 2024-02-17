8635cb8759c703a81f61bc9f6dc7b9adb89b3b1d
verticalsync <60797172+verticalsync@users.noreply.github.com>
2024-02-17 22:58:33 +0200

    feat(Experiments): Enable DeveloperActivtyShelf on plugin activation

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -444,6 +444,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "Fres",
         id: 843448897737064448n,
     },
+    happy_enderman: {
+        name: "Happy enderman",
+        id: 1083437693347827764n
+    },
 } satisfies Record<string, Dev>);
 
 export const SuncordDevs = /* #__PURE__*/ Object.freeze({
