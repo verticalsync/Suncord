aba0f0091f843b301bb3d517a184df9394251b13
verticalsync <60797172+verticalsync@users.noreply.github.com>
2024-03-14 18:33:15 +0200

    update(plugin): pinDms

--- src/plugins/pinDms/components/CreateCategoryModal.tsx
+++ src/plugins/pinDms/components/CreateCategoryModal.tsx
@@ -128,3 +128,4 @@ export function NewCategoryModal({ categoryId, modalProps, initalChannelId }: Pr
 
 export const openCategoryModal = (categoryId: string | null, channelId: string | null) =>
     openModal(modalProps => <NewCategoryModal categoryId={categoryId} modalProps={modalProps} initalChannelId={channelId} />);
+
