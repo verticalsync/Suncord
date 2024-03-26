2a9dc791a0b377e7b0213cb733abb050b43eb71d
verysillycat <cortex_@tutanota.com>
2024-03-26 02:37:49 -0600

    VencordToolbox ---> SuncordToolbox

--- src/suncordplugins/suncordToolbox/index.tsx
+++ src/suncordplugins/suncordToolbox/index.tsx
@@ -21,7 +21,7 @@ import "./index.css";
 import { openNotificationLogModal } from "@api/Notifications/notificationLog";
 import { Settings, useSettings } from "@api/Settings";
 import ErrorBoundary from "@components/ErrorBoundary";
-import { Devs } from "@utils/constants";
+import { Devs, SuncordDevs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findExportedComponentLazy } from "@webpack";
 import { Menu, Popout, useState } from "@webpack/common";
@@ -89,7 +89,7 @@ function VencordPopout(onClose: () => void) {
 function VencordPopoutIcon(isShown: boolean) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27" width={24} height={24}>
-            <path fill="currentColor" d={isShown ? "M9 0h1v1h1v2h1v2h3V3h1V1h1V0h1v2h1v2h1v7h-1v-1h-3V9h1V6h-1v4h-3v1h1v-1h2v1h3v1h-1v1h-3v2h1v1h1v1h1v3h-1v4h-2v-1h-1v-4h-1v4h-1v1h-2v-4H9v-3h1v-1h1v-1h1v-2H9v-1H8v-1h3V6h-1v3h1v1H8v1H7V4h1V2h1M5 19h2v1h1v1h1v3H4v-1h2v-1H4v-2h1m15-1h2v1h1v2h-2v1h2v1h-5v-3h1v-1h1m4 3h4v1h-4" : "M0 0h7v1H6v1H5v1H4v1H3v1H2v1h5v1H0V6h1V5h1V4h1V3h1V2h1V1H0m13 2h5v1h-1v1h-1v1h-1v1h3v1h-5V7h1V6h1V5h1V4h-3m8 5h1v5h1v-1h1v1h-1v1h1v-1h1v1h-1v3h-1v1h-2v1h-1v1h1v-1h2v-1h1v2h-1v1h-2v1h-1v-1h-1v1h-6v-1h-1v-1h-1v-2h1v1h2v1h3v1h1v-1h-1v-1h-3v-1h-4v-4h1v-2h1v-1h1v-1h1v2h1v1h1v-1h1v1h-1v1h2v-2h1v-2h1v-1h1M8 14h2v1H9v4h1v2h1v1h1v1h1v1h4v1h-6v-1H5v-1H4v-5h1v-1h1v-2h2m17 3h1v3h-1v1h-1v1h-1v2h-2v-2h2v-1h1v-1h1m1 0h1v3h-1v1h-2v-1h1v-1h1"} />
+            <path fill="currentColor" d={isShown ? "M9.00,4.25C9.00,5.08,9.68,6.31,10.50,7.00C11.33,7.68,12.00,9.24,12.00,10.45C12.00,12.72,8.24,17.00,6.25,17.00C4.41,17.00,3.63,19.15,5.12,20.10C5.88,20.58,8.45,20.98,10.83,20.98C14.29,21.00,15.74,20.41,18.08,18.08C21.76,14.39,21.82,10.93,18.25,7.01C15.13,3.58,9.00,1.76,9.00,4.25" : "M 7.64,3.89 C 3.94,6.27 2.45,9.64 3.19,14.02 4.52,18.33 7.44,20.61 11.95,20.86 15.74,20.73 18.47,18.98 20.16,15.61 21.88,10.68 20.59,6.78 16.27,3.89 17.83,3.89 19.39,3.89 20.95,3.89 20.94,5.58 20.95,7.27 21.00,8.95 21.97,9.94 22.96,10.91 23.95,11.86 23.95,11.92 23.95,11.98 23.95,12.05 22.96,13.00 21.97,13.97 21.00,14.95 20.95,16.95 20.94,18.95 20.95,20.95 18.95,20.94 16.95,20.95 14.95,21.00 13.97,21.97 13.00,22.96 12.05,23.95 11.98,23.95 11.92,23.95 11.86,23.95 10.91,22.96 9.94,21.97 8.95,21.00 6.95,20.95 4.95,20.94 2.95,20.95 2.97,18.95 2.95,16.95 2.91,14.95 1.93,13.97 0.95,13.00 -0.05,12.05 -0.05,11.98 -0.05,11.92 -0.05,11.86 0.95,10.91 1.93,9.94 2.91,8.95 2.95,7.27 2.97,5.58 2.95,3.89 4.52,3.89 6.08,3.89 7.64,3.89 Z M 15.61,3.61 C 15.82,3.73 16.04,3.82 16.27,3.89 20.59,6.78 21.88,10.68 20.16,15.61 18.47,18.98 15.74,20.73 11.95,20.86 7.44,20.61 4.52,18.33 3.19,14.02 2.45,9.64 3.94,6.27 7.64,3.89 7.87,3.82 8.09,3.73 8.30,3.61 8.84,3.39 9.41,3.22 9.98,3.09 11.91,2.71 13.79,2.89 15.61,3.61 Z M 11.86,-0.05 C 11.92,-0.05 11.98,-0.05 12.05,-0.05 13.23,1.17 14.42,2.39 15.61,3.61 13.79,2.89 11.91,2.71 9.98,3.09 9.41,3.22 8.84,3.39 8.30,3.61 9.48,2.39 10.67,1.17 11.86,-0.05 Z"} />
         </svg>
     );
 }
@@ -131,9 +131,9 @@ function ToolboxFragmentWrapper({ children }: { children: ReactNode[]; }) {
 }
 
 export default definePlugin({
-    name: "VencordToolbox",
-    description: "Adds a button next to the inbox button in the channel header that houses Vencord quick actions",
-    authors: [Devs.Ven, Devs.AutumnVN],
+    name: "SuncordToolbox",
+    description: "Adds a button next to the inbox button in the channel header that houses Suncord quick actions",
+    authors: [SuncordDevs.Cortex, Devs.Ven, Devs.AutumnVN],
 
     patches: [
         {
