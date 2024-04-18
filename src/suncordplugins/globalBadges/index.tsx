95530a04fa03da0c8fac87f8e6f0c90bdd38367e
verticalsync <60797172+verticalsync@users.noreply.github.com>
2024-04-19 01:47:01 +0300

    hi

--- src/suncordplugins/globalBadges/index.tsx
+++ src/suncordplugins/globalBadges/index.tsx
@@ -34,6 +34,7 @@ interface BadgeCache {
 }
 
 const API_URL = "https://clientmodbadges-api.herokuapp.com/";
+// const API_URL = "https://suncord-clientmodbadges-api.herokuapp.com/";
 
 const cache = new Map<string, BadgeCache>();
 const EXPIRES = 1000 * 60 * 15;
@@ -75,6 +76,7 @@ const GlobalBadges = ({ user }: { user: User; }) => {
 
     Object.keys(badges).forEach(mod => {
         if (mod.toLowerCase() === "vencord") return;
+        // if (mod.toLowerCase() === "suncord") return;
         badges[mod].forEach(badge => {
             if (typeof badge === "string") {
                 const fullNames = { "hunter": "Bug Hunter", "early": "Early User" };
