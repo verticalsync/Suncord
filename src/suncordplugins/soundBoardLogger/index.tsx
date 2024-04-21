981ab5598e5408c6219ec73d4692c5c48f6c510d
thororen <78185467+thororen1234@users.noreply.github.com>
2024-04-21 00:35:11 -0400

    SoundBoardLogger Fix :)

--- src/suncordplugins/soundBoardLogger/index.tsx
+++ src/suncordplugins/soundBoardLogger/index.tsx
@@ -1,28 +1,16 @@
 /*
- * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2023 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
+import { addChatBarButton, removeChatBarButton } from "@api/ChatButtons";
 import { disableStyle, enableStyle } from "@api/Styles";
-import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { FluxDispatcher } from "@webpack/common";
 
-import { IconWithTooltip, LogIcon } from "./components/Icons";
+import { IconWithTooltip, LogIcon, ChatBarIcon } from "./components/Icons";
 import { openSoundBoardLog } from "./components/SoundBoardLog";
 import settings from "./settings";
 import { updateLoggedSounds } from "./store";
@@ -45,14 +33,6 @@ export default definePlugin({
     settings,
     patches: [
         {
-            predicate: () => settings.store.IconLocation === "chat",
-            find: "ChannelTextAreaButtons",
-            replacement: {
-                match: /(\i)\.push.{1,30}disabled:(\i),.{1,20}\},"gift"\)\)/,
-                replace: "$&,(()=>{try{$2||$1.push($self.chatBarIcon(arguments[0]))}catch{}})()",
-            }
-        },
-        {
             predicate: () => settings.store.IconLocation === "toolbar",
             find: ".iconBadge}):null",
             replacement: {
@@ -69,15 +49,12 @@ export default definePlugin({
             await updateLoggedSounds(sound);
             getListeners().forEach(cb => cb());
         });
+        if (settings.store.IconLocation === "chat") addChatBarButton("vc-soundlog-button", ChatBarIcon);
     },
     stop() {
         disableStyle(styles);
+        if (settings.store.IconLocation === "chat") removeChatBarButton("vc-soundlog-button");
     },
-    chatBarIcon: (slateProps: any) => (
-        <ErrorBoundary noop>
-            <IconWithTooltip text="Open SoundBoard Log" icon={<LogIcon className="chatBarLogIcon" />} onClick={openSoundBoardLog} />
-        </ErrorBoundary>
-    ),
     toolbarPatch: obj => {
         if (!obj?.props?.children) return obj;
         obj.props.children = [<IconWithTooltip text="Open SoundBoard Log" icon={<LogIcon className="chatBarLogIcon" />} onClick={openSoundBoardLog} />, ...obj.props.children];
