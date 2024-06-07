efdfbe8e85eb992c06377b4a29ebdc23820c298b
verticalsync <60797172+verticalsync@users.noreply.github.com>
2024-06-08 00:44:47 +0300

    hhhh

--- scripts/generateReport.ts
+++ scripts/generateReport.ts
@@ -34,7 +34,6 @@ for (const variable of ["DISCORD_TOKEN", "CHROMIUM_BIN"]) {
 }
 
 const CANARY = process.env.USE_CANARY === "true";
-const PTB = process.env.USE_PTB === "true";
 
 const browser = await pup.launch({
     headless: "new",
@@ -84,7 +83,7 @@ function toCodeBlock(s: string) {
 async function printReport() {
     console.log();
 
-    console.log("# Suncord Report" + (CANARY ? " (Canary)" : PTB ? " (PTB)" : ""));
+    console.log("# Suncord Report" + (CANARY ? " (Canary)" : ""));
 
     console.log();
 
@@ -133,7 +132,7 @@ async function printReport() {
             },
             body: JSON.stringify({
                 description: "Here's the latest Suncord Report!",
-                username: "Suncord Reporter" + (CANARY ? " (Canary)" : PTB ? " (PTB)" : ""),
+                username: "Suncord Reporter" + (CANARY ? " (Canary)" : ""),
                 avatar_url: "https://raw.githubusercontent.com/verticalsync/Suncord/main/src/assets/icon.png",
                 embeds: [
                     {
@@ -313,4 +312,4 @@ await page.evaluateOnNewDocument(`
     }
 `);
 
-await page.goto(CANARY ? "https://canary.discord.com/login" : PTB ? "https://ptb.discord.com/login" : "https://discord.com/login");
+await page.goto(CANARY ? "https://canary.discord.com/login" : "https://discord.com/login");
