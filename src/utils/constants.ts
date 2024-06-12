c1aa412041ddb0aab94607824ee7aee14f3f7a86
verticalsync <60797172+verticalsync@users.noreply.github.com>
2024-06-12 10:52:25 +0300

    huh

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -33,6 +33,10 @@ export interface Dev {
  * If you are fine with attribution but don't want the badge, add badge: false
  */
 export const Devs = /* #__PURE__*/ Object.freeze({
+    None: {
+        name: "None",
+        id: 0n
+    },
     Ven: {
         name: "Vee",
         id: 343383572805058560n
